df212c34c66615cad2c269daa05a3ff14545b515
muhametii00 <konushevcimuhamet@gmail.com>
2023-01-24 22:29:05 +0100

    Created a view using props
    Created a reusable component called Task.js and I passed text as a prop to the main window

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import NewTask from "./components/NewTask";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      </View>
+      <View style={styles.items}>
+        <Task text=" Learn React JS" />
+        <Task text=" Learn React Native" />
+      </View>
+      <NewTask text="Add New Task" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
---
+++ components/NewTask.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+
+const NewTask = (props) => {
+  return <View style={styles.item}></View>;
+};
+
+const styles = StyleSheet.create({});
+export default NewTask;
