23751f77f4dccd8e765cceb728f619a0bfe366f1
muhametii00 <konushevcimuhamet@gmail.com>
2023-01-25 22:34:59 +0100

    Added functionality to NewTask
    Had to make them all in one component because of the useState hook on adding new task and also delete them

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import {
+  Keyboard,
   KeyboardAvoidingView,
   StyleSheet,
   Text,
@@ -6,22 +8,55 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import NewTask from "./components/NewTask";
 import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask("");
+  };
+
+  const CompleteTask = (index) => {
+    let copyItems = [...taskItems];
+    copyItems.splice(index, 1);
+    setTaskItems(copyItems);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
 
         <View style={styles.items}>
-          <Task text=" Learn React JS" />
-          <Task text=" Learn React Native" />
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => CompleteTask()}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
 
-      <NewTask />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -41,5 +76,34 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  addWrapper: {
+    width: 55,
+    height: 55,
+    backgroundColor: "#FFF",
+    borderColor: "#C0C0C0",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    borderWidth: 1,
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    backgroundColor: "#FFF",
+  },
+  addText: {
+    fontSize: 20,
   },
 });
